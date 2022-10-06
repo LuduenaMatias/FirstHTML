a5491ada9329410e3e941e6cb022af23a472c552
Luduena Matias <luduena.matias96@gmail.com>
2022-10-06 11:22:50 -0300

    Primer Repositorio

---
+++ Script.js
@@ -0,0 +1,150 @@
+// Variables
+	info = "Info Asfixia : ¿Qué es?  \n La asfixia se produce cuando quedan atrapados en la garganta alimentos u objetos pequeños de tal manera que obstruyen las vías respiratorias. \n También puede producirse por la inhalación de humo o gases tóxicos. \n La asfixia impide que el oxígeno llegue a los pulmones y desde allí al resto de los órganos. Si el cerebro permanece sin oxígeno durante más de cuatro minutos, puede ocurrir algún daño cerebral o la muerte.\n ¿Cómo está la persona? \n El signo universal de sufrimiento en caso de asfixia es agarrarse la garganta con la mano.Otros signos comunes 'son:' \n • Piel morada \n • Dificultad para respirar \n • Incapacidad para hablar \n • Pérdida del conocimiento si la obstrucción no se alivia \n • Respiración ruidosa o sonidos muy agudos al inhalar \n • Tos débil e improductiva"
+	
+	procedimiento = "Procedimiento : ¿Qué hacer? \n Si la persona está tosiendo con fuerza y puede hablar, NO aplique primeros auxilios ya que una tos fuerte puede desalojar el objeto.En cambio, si la persona no puede respirar a causa de un bloqueo de las vías respiratorias con un trozo de alimento u otro objeto, se debe aplicar la Maniobra de Heimlich: \n  1. Párese detrás de la persona y rodéela con los brazos por la cintura. \n 2. Forme un puño con una mano y coloque el puño por el lado del pulgar justo encima del ombligo de la persona, bien por debajo del esternón. \n 3. Agarre el puño con la otra mano. \n 4. Realice compresiones rápidas hacia arriba y hacia adentro con el puño. \n 5. Continúe con dichas compresiones hasta que el objeto salga o la víctima pierda el conocimiento. Es posible que se deba repetir el procedimiento varias veces antes de lograr desalojar el objeto. \n 6. Si la persona pierde el conocimiento, bájela al suelo, llame al número de emergencias y comience a realizar reanimación cardiopulmonar (RCP). \n Esta técnica de emergencia se puede utilizar de manera segura tanto en niños mayores de 1 años como en adultos.\n En personas embarazadas o con problemas de sobrepeso: \n • Rodee a la persona con los brazos por el PECHO.\n • Coloque el puño en la MITAD del esternón entre los senos.\n • Realice compresiones firmes hacia atrás.Después de extraer el objeto que causó la asfixia, mantenga a la persona inmóvil y consiga ayuda médica."
+
+	inhala = "En caso de asfixia por humo o inhalación tóxica: \n 1. Llame al número de emergencias.\n 2. Sitúe a la víctima en un lugar alejado del peligro, donde pueda respirar aire fresco.\n 3. Si la persona presenta coloración morada en los labios, orejas y uñas, debe practicarle respiración boca a boca.\n 4. Si permanece inconsciente o ha respirado sustancias tóxicas, trasládelo de manera urgente al centro de salud u hospital más cercano."
+
+
+	
+	infoP = "Info Paro Cardiorespiratorio : ¿Qué es? \n Durante un paro cardiorrespiratorio el corazón deja de latir y la respiración cesa unos segundos después. La sangre deja de circular y por lo tanto deja de haber suministro de oxígeno al cerebro, corazón y pulmones. \n \n ¿Cuáles son las causas? \n Las principales causas del paro cardiorrespiratorio son: \n  Ataque cardíaco. \n  Hipotermia profunda. \n  Shock. \n  Traumatismo en la cabeza. \n  Electrocución. \n  Hemorragias severas. \n  Deshidratación. \n \n ¿Cómo está la persona? \n Cuando alguien sufre un paro cardiorrespiratorio presenta los siguientes síntomas: \n  No respira. \n  Piel pálida o morada, especialmente en labios y uñas. \n  Pérdida de conocimiento. \n  Pupila dilatada parcialmente. A los 2 o 3 minutos la dilatación es total y no hay reacción a la luz. \n"
+
+	procedimientoP = "Procedimiento : ¿Qué hacer? \n Si una persona sufre un paro cardiorrespiratorio, además de llamar inmediatamente al servicio de emergencias, las maniobras d reanimación cardiopulmonar (RCP) pueden salvarle la vida y evitar posibles lesiones. \n \n RCP en niños mayores, adolescentes y adultos: \n 1. Coloque a la víctima sobre el suelo. Abra la vía aérea: extienda la cabeza de la persona hacia atrás y ábrale la boca. Retire los cuerpos extraños que pudieran obstruir la respiración.\n 2. Localice el centro del esternón entre los senos, en el medio del pecho. Coloque el talón de una mano, luego la otra mano encima y entrelace los dedos (cuidando de no apoyarse sobre las costillas). \n 3. Adopte una postura erguida para que los hombros permanezcan perpendicularmente encima del esternón del accidentado y con las manos aplique presión suave, cargando el peso del cuerpo sobre los brazos rectos de manera que el esternón baje unos 5 cm. Es importante no doblar los codos para ejercer la presión suficiente y reducir el cansancio. \n 4. Realice 30 compresiones, a un ritmo de 2 por segundo.\n 5. Tape la nariz de la persona con sus dedos y sople suavemente sobre su boca 2 veces (ventilaciones) de 1 segundo cada una.6. Repita la secuencia: 30 compresiones y 2 ventilaciones (5 ciclos o 2 minutos) hasta que la víctima recupere la consciencia o llegue el servicio de emergencias."
+
+
+
+	infoQ = "Info Quemaduras : ¿Qué es? \n La quemadura es una lesión en la piel que puede ser causada por distintos factores: algo caliente, fuego, agentes químicos, electrocución, entre otros.Las quemaduras se clasifican en tipo A, B, AB según las capas de la piel y los tejidos profundos lesionadosUna quemadura grave puede poner en peligro la vida y requiere atención médica inmediata. \n \n Se consideran quemaduras graves: \n • Las que dificultan la respiración. \n • Las que cubren más de una parte del cuerpo. \n • Las quemaduras en la cabeza, cuello, manos, pies o genitales.\n • Las quemaduras en un niño o un anciano. \n • Las quemaduras AB (las más profundas). \n • Las quemaduras causadas por electricidad, sustancias químicas o explosiones. \n"
+
+	nohacer = "Otros : Qué NO hacer \n Hay ciertas acciones que en ningún caso se deben llevar a cabo, porque pueden perjudicar el cuadro del paciente. Por eso es importante frente a quemaduras graves: \n • No aplicar ungüentos, manteca, hielo, medicamentos, cremas, pomadas, aceites en aerosol, pasta dental ni cualquier otro remedio casero sobre la quemadura. Sólo agua. \n • No soplar sobre la quemadura, ni retirar la ropa o cualquier otro elemento que esté pegado a la piel. \n • No tocar la piel muerta ni romper las ampollas, pues el líquido que contienen protege de una posible infección. \n • No intentar apagar las llamas sobre el cuerpo con agua. Siempre se deben apagar haciendo que la persona ruede o cubriéndolas con una manta. \n • No sumergir una quemadura grave en agua fría, pues esto puede causar shock. \n • No colocar una almohada debajo de la cabeza de la persona si hay quemaduras de las vías respiratorias, porque esto puede cerrarlas. \n"
+
+	procedimientoQ = "Procedimiento : Qué hacer \n • Eliminar la causa de la quemadura. Si la persona tiene el cuerpo en llamas, se la debe hacer rodar por el piso para apagarlas. \n • Si no hay roturas en la piel, refrescar la zona quemada con agua fría en abundancia durante al menos 15 minutos. Una toalla limpia, húmeda y fría también ayuda a reducir el dolor. \n • Envolver la lesión con gasas o paños limpios, humedecidos en agua. El vendaje tiene que estar flojo. \n • Si el dolor persiste, aparecen ampollas, un aspecto 'carbonizado', o el tamaño de la quemadura es superior al de la palma de la mano, es necesario llevar a la víctima a un centro de salud o llamar al servicio de emergencias."
+//---------------------------------------------------------------------------------------------------
+//Aqui ira el Javascript
+
+		function cambio(pagina){
+
+			document.home.action = pagina;
+
+		}
+
+		function botonero(){
+
+//--------------------------------------------------------------------------------------------------
+//Toma el Valor del radio button
+
+			var elegido = document.getElementsByName("radOptions");
+
+			for(i=0;i<elegido.length;i++) {
+				if (elegido[i].checked) { 
+					valor =elegido[i].value; 
+					break; 
+				}
+			}
+
+//---------------------------------------------------------------------------------------------------
+			
+
+			if (valor == "InfoAsf") {
+				alert(info);
+
+			} else if (valor == "ProcedimientoAsf") {
+
+				alert(procedimiento);
+
+			}else if (valor == "AdicionalAsf") {
+
+				alert(inhala);
+
+			};
+			
+		
+
+//---------------------------------------------------------------------------------------------------
+			
+
+			if (valor == "InfoParo") {
+				alert(infoP);
+
+			} else if (valor == "ProcedimientoParo") {
+
+				alert(procedimientoP);
+			};
+
+//---------------------------------------------------------------------------------------------------
+			
+
+			if (valor == "InfoQuema") {
+				alert(infoQ);
+
+			} else if (valor == "NoHacer") {
+
+				alert(nohacer);
+
+			}else if (valor == "ProcedimientoQ") {
+
+				alert(procedimientoQ);	
+
+			};
+
+		};
+
+//---------------------------------------------------------------------------------------------------
+
+		function ModalImagen(imagenlink){
+
+			var fondo = document.createElement("div");
+			var modal = document.createElement("div");
+
+			var imagen = document.createElement("img");
+			imagen.src = imagenlink;
+			imagen.style.with = "80%";
+
+			fondo.appendChild(modal);
+			modal.appendChild(imagen);
+			fondo.className = "transparencia" ;
+			modal.className = "modal";
+
+			var cerrar = document.createElement("div");
+			var x = document.createTextNode("X");
+			cerrar.appendChild(x);
+			cerrar.className = "cierre";
+
+			cerrar.addEventListener("click",function(){ 
+				fondo.style.visibility = "hidden";
+			})
+
+			modal.appendChild(cerrar);
+			document.body.appendChild(fondo);
+
+			return fondo;
+
+		}
+
+				function crearModalTexto(msj){
+				var f = document.createElement("div");
+				var m = document.createElement("div");
+				var t = document.createTextNode(msj);
+				f.appendChild(m);
+				m.appendChild(t);
+				f.className = "contenedor";
+				m.className = "modal";
+				var cerrar = document.createElement("div");
+				var x = document.createTextNode("X");
+				cerrar.appendChild(x);
+				cerrar.className = "cerrar";
+				cerrar.addEventListener("click", function(){
+					f.style.visibility = "hidden";
+				});
+				m.appendChild(cerrar);
+				document.body.appendChild(f);
+				return f;
+			}
+
+		function mostrarModal(obj){
+
+			obj.style.visibility = "visible";
+
+		}
+
+		var mimodal = crearModalTexto(" Texto random de prueba");
+		var mimodalimagen = ModalImagen("fuego.jpg");
